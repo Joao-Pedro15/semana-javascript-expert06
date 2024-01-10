e9375869dd75a83162b472dd9bb60271faf04206
João Pedro <jjoao.monteiro15@gmail.com>
2024-01-10 19:18:08 -0300

    criando rota stream para ser consumida no front

--- server/routes.js
+++ server/routes.js
@@ -39,6 +39,16 @@ async function routes(request, response) {
     return stream.pipe(response)
   }
 
+  if (method === 'GET' && url.includes('/stream')) {
+    const { onClose, stream } = controller.createClientStream()
+    request.once('close', onClose)
+    response.writeHead(200, {
+      'Content-Type': 'audio/mpeg',
+      'Accept-Rages': 'bytes'
+    })
+    return stream.pipe(response)
+  }
+
   //files
   if (method === 'GET') {
     const {
